df03f76453341de954d93b96724741faf762e6f8
khamiltonuk <krisham87@hotmail.com>
2018-04-29 18:36:43 +0200

    connected to the db

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const passport = require("passport");
+const app = express();
+
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};
---
+++ services/passport.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log("access Token", accessToken);
+      // console.log("refresh Token", refreshToken);
+      console.log("profile", profile);
+      User.findOne({
+        googleId: profile.id
+      }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
